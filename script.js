68efd1434aec0f2639a50e2d1f7bcc1c29bbb7c3
edwinndungu2 <edwin.ndungu2@student.moringaschool.com>
2025-12-18 13:53:48 +0300

    new edit

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function order(product){
 const phone="254706230252"; // REPLACE
-const msg=`Hello EDTECH SYSTEMS, I am interested in ${product}.`;
+const msg=`Hello EDTECH SYSTEMS, Please arrange to deliver ${product}.`;
 window.open(`https://wa.me/${phone}?text=${encodeURIComponent(msg)}`,'_blank');
 }
