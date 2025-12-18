3d47514d491361c4e12631264c1e1d4eea215770
edwinndungu2 <edwin.ndungu2@student.moringaschool.com>
2025-12-18 13:56:46 +0300

    change in script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function order(product){
 const phone="254706230252"; // REPLACE
-const msg=`Hello EDTECH SYSTEMS, Please arrange to deliver the ${product}.`;
+const msg=`Hello EDTECH SYSTEMS, Please arrange to deliver the ${product} as specified.`;
 window.open(`https://wa.me/${phone}?text=${encodeURIComponent(msg)}`,'_blank');
 }
