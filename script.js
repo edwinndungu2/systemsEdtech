f1c8b51a08569756eee340d7c54e7346ad1da3cc
edwinndungu2 <edwin.ndungu2@student.moringaschool.com>
2025-12-18 14:13:33 +0300

    hamburger button

--- script.js
+++ script.js
@@ -3,3 +3,7 @@ const phone="254706230252"; // REPLACE
 const msg=`Hello EDTECH SYSTEMS, Please arrange to deliver the ${product} as specified.`;
 window.open(`https://wa.me/${phone}?text=${encodeURIComponent(msg)}`,'_blank');
 }
+
+function toggleMenu(){
+  document.querySelector("nav").classList.toggle("active");
+}
