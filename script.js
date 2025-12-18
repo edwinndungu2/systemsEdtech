ebc7420d1e05af1091968d50e71f890ff37b4420
edwinndungu2 <edwin.ndungu2@student.moringaschool.com>
2025-12-18 16:38:00 +0300

    CHANGE IN NAME in script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function order(product){
 const phone="254706230252"; // REPLACE
-const msg=`Hello EDTECH SYSTEMS, Please arrange to deliver the ${product} as specified.`;
+const msg=`Hello EDVOLTA, Please arrange to deliver the ${product} as specified.`;
 window.open(`https://wa.me/${phone}?text=${encodeURIComponent(msg)}`,'_blank');
 }
 
